6440e1cf3dbb9c3d9a42cf543d7268f8d94bd0fa
irfansayyed101 <irfansayyed101@gmail.com>
2020-03-04 15:32:48 +0530

    Landing page

---
+++ src/common/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../components/css/header.css';
+const LandingPage = (props) => {
+    return (
+        <div>
+            <div className="logo">
+                <img className="bg" src="https://freighthub.com/wp-content/uploads/2019/09/Background-pic.jpg" title="ANZ Logo" alt="ANZ Logo"/> 
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage;
